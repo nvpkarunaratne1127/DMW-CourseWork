4d1df63f317c07b086a0c67b37c2aff1309e1b61
nvpkarunaratne1127 <nvpkarunaratne@gmail.com>
2026-07-26 18:27:57 +0530

    Add footer to the main HTML structure

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    &copy; <?php echo date('Y'); ?> Campus Library &mdash; Student Book Reservation System
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
